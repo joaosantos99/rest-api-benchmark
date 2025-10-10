7a40a6b42f9ce4338d82729cbd1a27c7af05e76b
João Santos <68588265+joaosantos99@users.noreply.github.com>
2025-10-10 21:22:31 +0100

    feat: add piDigits test to bun

--- services/bun/src/index.ts
+++ services/bun/src/index.ts
@@ -1,11 +1,13 @@
 import helloWorld from "./tests/hello-world";
+import piDigits from "./tests/pi-digits";
 
 const port = process.env.PORT || 8080;
 
 Bun.serve({
   port: port,
   routes: {
-    '/api/hello-world': () => helloWorld()
+    '/api/hello-world': () => helloWorld(),
+    '/api/pi-digits': () => piDigits(),
   },
 });
 
