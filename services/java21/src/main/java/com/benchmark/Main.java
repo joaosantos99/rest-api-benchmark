c98c537bf96838af588589ba05e2841e3d3a4c07
João Santos <68588265+joaosantos99@users.noreply.github.com>
2025-10-19 21:47:00 +0100

    feat: java 21 service
    - base for http server
    - hello world test
    - n-body test
    - Dockerfile

---
+++ services/java21/src/main/java/com/benchmark/Main.java
@@ -0,0 +1,30 @@
+package com.benchmark;
+
+import com.benchmark.tests.HelloWorld;
+import com.benchmark.tests.NBody;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080";
+        }
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(port)), 0);
+
+        server.createContext("/api/hello-world", new HelloWorld());
+        server.createContext("/api/n-body", new NBody());
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server running at http://localhost:" + port + " (Java 21)");
+    }
+}
